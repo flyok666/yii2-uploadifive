34176a8d2dcf66d929bd635872b88cb85968158c
flyok666 <flyok666@qq.com>
2017-07-18 21:50:01 +0800

    Update UploadifiveAsset.php

--- UploadifiveAsset.php
+++ UploadifiveAsset.php
@@ -8,8 +8,8 @@ class UploadifiveAsset extends AssetBundle {
 
     public $sourcePath = '@vendor/flyok666/yii2-uploadifive/assets';
     public $basePath = '@webroot/assets';
-    public $css = ['uploadify.css'];
-    public $js = ['jquery.uploadify.js'];
+    public $css = ['uploadifive.css'];
+    public $js = ['jquery.uploadifive.js'];
     public $depends = ['yii\web\JqueryAsset'];
 
 }
